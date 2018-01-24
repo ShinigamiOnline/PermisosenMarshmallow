ebe109fc29cece2ba52e19e087979451182ac8a8
Shinigami <denshaotoko@outlook.com>
2018-01-24 20:39:36 +0100

    commitClase

--- app/src/main/java/permisosenmarshmallow/example/org/permisosenmarshmallow/MainActivity.java
+++ app/src/main/java/permisosenmarshmallow/example/org/permisosenmarshmallow/MainActivity.java
@@ -1,15 +1,25 @@
 package permisosenmarshmallow.example.org.permisosenmarshmallow;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.provider.CallLog;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int SOLICITUD_PERMISO_WRITE_CALL_LOG = 0 ;
+    private View vista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +36,53 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        vista = findViewById(R.id.content_main);
 
+
+    }
+
+    void borrarLlamada() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_CALL_LOG) == PackageManager.PERMISSION_GRANTED) {
+            getContentResolver().delete(CallLog.Calls.CONTENT_URI, "number='555555555'", null);
+            Snackbar.make(vista, "llamadas borradas del registro.", Snackbar.LENGTH_SHORT).show();
+        } else {
+            solicitarPermiso(Manifest.permission.WRITE_CALL_LOG, "Sin el permiso" + "administrar llamadas no puedo borrar llamadas del registro.",
+                    SOLICITUD_PERMISO_WRITE_CALL_LOG, this);
+        }
+
+    }
+
+    public static void solicitarPermiso(final String permiso, String justificacion, final int requestCode, final Activity actividad) {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(actividad,
+                permiso)) {
+            new AlertDialog.Builder(actividad)
+                    .setTitle("Solicitud de permiso")
+                    .setMessage(justificacion)
+                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int whichButton) {
+                            ActivityCompat.requestPermissions(actividad,
+                                    new String[]{permiso}, requestCode);
+                        }
+                    })
+                    .show();
+        } else {
+            ActivityCompat.requestPermissions(actividad,
+                    new String[]{permiso}, requestCode);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions, int[] grantResults) {
+        if (requestCode == SOLICITUD_PERMISO_WRITE_CALL_LOG) {
+            if (grantResults.length == 1 &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                borrarLlamada();
+            } else {
+                Toast.makeText(this, "Sin el permiso, no puedo realizar la " +
+                        "acción", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     @Override
@@ -50,4 +106,6 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
